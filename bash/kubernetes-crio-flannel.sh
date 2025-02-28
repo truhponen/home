fcc3eaff1b6de2ba357a108b266427f215594b81
truhponen <truhponen@iki.fi>
2025-02-28 09:44:53 +0200

    Update kubernetes-crio-flannel.sh

--- bash/kubernetes-crio-flannel.sh
+++ bash/kubernetes-crio-flannel.sh
@@ -49,3 +49,8 @@ echo "|"
 echo "Init"
 echo "|"
 kubeadm init --pod-network-cidr=10.244.0.0/16
+
+echo "|"
+echo "Flannel CNI"
+echo "|"
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
