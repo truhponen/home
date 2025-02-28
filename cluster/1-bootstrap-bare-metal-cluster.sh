c83bcb5a17d0fe3165cbd297dc168e84c37d9ba9
truhponen <truhponen@iki.fi>
2025-02-28 23:22:33 +0200

    Update 1-bootstrap-bare-metal-cluster.sh

--- cluster/1-bootstrap-bare-metal-cluster.sh
+++ cluster/1-bootstrap-bare-metal-cluster.sh
@@ -38,7 +38,7 @@ apt-mark hold cri-o kubelet kubeadm kubectl
 echo "|"
 echo "Install Helm with apt"
 echo "|"
-curl -LO https://git.io/get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
