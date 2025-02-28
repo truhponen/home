8d937787507345f1d5b0eba181912f352f436aad
truhponen <truhponen@iki.fi>
2025-03-01 00:13:52 +0200

    Update 3-add-cluster-applications.sh

--- cluster/3-add-cluster-applications.sh
+++ cluster/3-add-cluster-applications.sh
@@ -12,14 +12,6 @@ echo "|"
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 echo "|"
-echo "Install Flannel container networking with Helm"
-echo "|"
-kubectl create ns kube-flannel
-kubectl label --overwrite ns kube-flannel pod-security.kubernetes.io/enforce=privileged
-helm repo add flannel https://flannel-io.github.io/flannel/
-helm install --set podCidr="10.244.0.0/16" --namespace kube-flannel flannel flannel/flannel
-
-echo "|"
 echo "Innitialize Helm, i.e. install required cluster side applications"
 echo "|"
 helm init
@@ -29,14 +21,14 @@ echo "Install PureLB bare metal loadbalancer with Helm"
 echo "|"
 helm repo add purelb https://gitlab.com/api/v4/projects/20400619/packages/helm/stable
 helm repo update
-helm install - https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/purelb/helm-customization.yaml --create-namespace --namespace=purelb purelb purelb/purelb
+helm install purelb purelb/purelb -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/purelb/helm-customization.yaml --create-namespace -n purelb
 
 echo "|"
 echo "Install Traefik Ingress controller with Helm and customizations"
 echo "|"
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
-helm install -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/traefik/helm-customization.yaml --create-namespace --namespace=traefik traefik traefik/traefik
+helm install traefik traefik/traefik -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/traefik/helm-customization.yaml --create-namespace -n traefik
 
 echo "|"
 echo "Install CSI-driver NFS with Helm"
