30cea41fcb38c912d4a79879f1c240f8c83eb7d2
truhponen <truhponen@iki.fi>
2025-03-15 22:31:16 +0200

    Update 0-cluster-basics.sh

--- cluster/0-cluster-basics.sh
+++ cluster/0-cluster-basics.sh
@@ -5,18 +5,16 @@
 # curl https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/0-cluster-basics.sh | bash
 
 echo "|"
+echo "Add Git-secret repository"
+echo "|"
+curl -fsSL https://gitsecret.jfrog.io/artifactory/api/gpg/key/public | gpg --dearmor -o /etc/apt/keyrings/git-secret-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/git-secret-apt-keyring.gpg] https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main" | tee /etc/apt/sources.list.d/git-secret.list
+
+echo "|"
 echo "Install basic programs with apt"
 echo "|"
 apt update
-apt install -y git gpg wget net-tools
-
-echo "|"
-echo "Install Git-secret"
-echo "|"
-sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
-wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
-apt update
-apt install -y git-secret
+apt install -y git gpg wget net-tools git-secret
 
 echo "|"
 echo "Testing, that it worked"
