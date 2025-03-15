937df88cb3f066981792510f8c76994a5a37e09f
truhponen <truhponen@iki.fi>
2025-03-15 22:05:35 +0200

    Update and rename 3-add-cluster-applications.sh to 4-add-cluster-applications.sh

--- cluster/4-add-cluster-applications.sh
+++ cluster/4-add-cluster-applications.sh
@@ -18,14 +18,14 @@ echo "Install PureLB bare metal loadbalancer with Helm"
 echo "|"
 helm repo add purelb https://gitlab.com/api/v4/projects/20400619/packages/helm/stable
 helm repo update
-helm install purelb purelb/purelb -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/purelb/helm-customization.yaml --create-namespace -n purelb
+helm install purelb purelb/purelb -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/purelb/helm-customization.yaml --create-namespace --namespace purelb
 
 echo "|"
 echo "Install Traefik Ingress controller with Helm and customizations"
 echo "|"
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
-helm install traefik traefik/traefik -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/traefik/helm-customization.yaml --create-namespace -n traefik
+helm install traefik traefik/traefik -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/traefik/helm-customization.yaml --create-namespace --namespace traefik
 
 echo "|"
 echo "Install CSI-driver NFS with Helm"
@@ -33,4 +33,12 @@ echo "There might be errors as instructions install CSI to kube-system namespace
 echo "|"
 helm repo add csi-driver-nfs https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts
 helm repo update
-helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --create-namespace --namespace=csi-driver-nfs --version v4.10.0
+helm install csi-driver-nfs csi-driver-nfs/csi-driver-nfs --create-namespace --namespace csi-driver-nfs --version v4.10.0
+
+echo "|"
+echo "Install Kubernetes dashboards"
+echo "ThisInstall Kubernetes dashboards"
+echo "|"
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
+helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard -f https://raw.githubusercontent.com/truhponen/home/refs/heads/main/cluster/kubernetes-dashboard/helm-customization.yaml --create-namespace --namespace kubernetes-dashboard
