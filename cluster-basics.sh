3cf4f928b46d6f48812d14cae231138c3a0cc0bd
truhponen <truhponen@iki.fi>
2025-03-15 22:13:35 +0200

    Update cluster-basics.sh

--- cluster-basics.sh
+++ cluster-basics.sh
@@ -8,4 +8,14 @@ echo "|"
 echo "Install basic programs with apt"
 echo "|"
 apt update
-apt install -y wget net-tools
+apt install -y git gpg wget net-tools
+
+echo "|"
+echo "Install Git-secret"
+echo "|"
+sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
+wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y git-secret
+
+# Testing, that it worked:
+git secret --version
